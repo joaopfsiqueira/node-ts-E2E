2a8c1ee6e10b561056203566f22ee1d8b87451c1
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-09-23 22:05:37 -0300

    feat: criando schema

---
+++ src/infra/mongoose/models/user.ts
@@ -0,0 +1,18 @@
+import { model, Schema } from 'mongoose';
+import { IUser } from '../../../utils/interfaces/repository/user-collection.interface';
+
+const UserSchema = new Schema<IUser>(
+	{
+		name: { type: String, required: true },
+		username: { type: String, required: true },
+		password: { type: String, required: true },
+		created_at: { type: Date, required: true },
+	},
+	{
+		versionKey: false,
+	},
+);
+
+UserSchema.index({ username: 1 });
+
+export default model<IUser>('Users', UserSchema);
