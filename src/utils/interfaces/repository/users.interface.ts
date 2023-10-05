565915988c97e13df5c51f4b62dea4829fe705fb
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-10-04 21:49:48 -0300

    fix: ajustando interface

--- src/utils/interfaces/repository/users.interface.ts
+++ src/utils/interfaces/repository/users.interface.ts
@@ -2,5 +2,5 @@ import { IUser } from '../infra/models/user-collection.interface';
 
 export interface IUserRepository {
 	create(name: string, username: string, password: string, created_at: Date): Promise<IUser>;
-	findByUsername(username: string): Promise<IUser>;
+	findByUsername(username: string): Promise<IUser | Boolean>;
 }
